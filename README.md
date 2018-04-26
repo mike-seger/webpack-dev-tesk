44cfc65b69212e8366ce6e4b9e8c16e0925ad499
mike-seger <mss@unknown.com>
2018-04-26 21:04:05 +0200

    removed node_modules

---
+++ README.md
@@ -0,0 +1,1 @@
+npm i webpack webpack-cli webpack-dev-server html-webpack-plugin style-loader css-loader babel-core babel-loader babel-preset-env babel-preset-react -D
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './styles.css';
 const MyComponent = () => (
-    <h1>Hello From A New Component</h1>
+    <h1>Hello From A New Component 2</h1>
 );
 export default MyComponent;
